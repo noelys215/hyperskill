8806be392f78a85ba42f94ced6b34e815c9a2d44
Henry Betancourth <69738617+noelys215@users.noreply.github.com>
2024-01-07 22:12:54 -0500

    first commit

--- Topics/Arrays as parameters/Adding values/src/Main.java
+++ Topics/Arrays as parameters/Adding values/src/Main.java
@@ -1,8 +1,15 @@
-import java.util.*;
-
 public class Main {
+
     public static void main(String[] args) {
-        char ch = 937;
-        System.out.println("\r" + ch);
+        Patient patient1 = new Patient("Heinrich", 40, 182.0f);
+        Patient patient2 = new Patient("Mary", 33, 171.5f);
     }
+}
+
+class Patient {
+    String name;
+    int age;
+    float height;
+
+
 }
